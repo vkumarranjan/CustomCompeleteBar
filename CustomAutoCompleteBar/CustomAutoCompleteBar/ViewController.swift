f3cf2d44897779ee4893246bfd4a67f9a917d39a
Vinay Nation <vinay.raj.apple@gmail.com>
2017-06-12 15:40:24 +0530

    First commit

---
+++ CustomAutoCompleteBar/CustomAutoCompleteBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomAutoCompleteBar
+//
+//  Created by ARC MAC on 12/06/17.
+//  Copyright © 2017 Vinay Nation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
